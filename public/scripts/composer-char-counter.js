ed77452e265f3d622559dbe8ab6bf83e5ad085e8
sumedha <sumedhanarayanan@gmail.com>
2019-01-22 22:34:20 +0000

    Modify the composer char function to remove red styling if characters are deleted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   let tweet = "textarea[name='composeTweet']";
   $(tweet).on("input", function() {
     let charCounter = 140 - $(this).val().length;
-    $(this).siblings(".counter").text(charCounter);
+    let counter = $(this).siblings(".counter");
+    counter.text(charCounter);
     if(charCounter < 0) {
-      // $(this).parent().children(".counter").css("color", "red");
-      $(this).siblings(".counter").addClass("invalid");
+      counter.addClass("invalid");
+    } else {
+      counter.removeClass("invalid");
     }
   });
 });
